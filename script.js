6ea3bccc0655d1c5ac6c102cc7ffc606e74b1240
BallsSweden <87786835+BallsSweden@users.noreply.github.com>
2021-10-07 15:58:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number!'
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// document.querySelector('.guess')
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const randomGenerator = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = randomGenerator;
 let score = 4;
 let highscore = 0;
 
+function displayMessage(message) {
+  return document.querySelector('.message').textContent = message;
+};
+
+function displayNumber(number) {
+  return document.querySelector('.number').textContent = number;
+};
+
+function backgroundColor(color) {
+  return document.querySelector('body').style.backgroundColor = color;
+};
+
+function widthNumber(width) {
+  return document.querySelector('.number').style.width = width;
+};
+
+//game logic
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //No input
   if (!guess) {
-    document.querySelector('.message').textContent = "Pick a fuckin' number!"
+    displayMessage("Pick a fuckin' number")
     //Correct guess
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = `Yep, I have ${secretNumber} cocks. You ever blow ${secretNumber} loads at once? No? How womanly.`
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-
+    displayMessage(`Yep, I have ${secretNumber} cocks. You ever blow ${secretNumber} loads at once? No? How womanly.`)
+    displayNumber(secretNumber);
+    backgroundColor('#60b347');
+    widthNumber('30rem');
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  }
-  //Guess too high
-  else if (guess > secretNumber) {
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = "Guess is too high you fucking stoner";
+      displayMessage(guess > secretNumber ? "Guess is too high you fuckin' stoner" : "Guess is too low. Suck my dicks while you're down there");
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = "YOU LOST! TRASH, GET OUT AND NEVER GUESS HOW MANY DICKS I HAVE EVER AGAIN";
+      displayMessage("YOU LOST! TRASH, GET OUT AND NEVER GUESS HOW MANY DICKS I HAVE EVER AGAIN");
       document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#ff4d4d';
-    }
-    //Guess too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = "Guess is too low. Suck my dicks while you're down there";
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = "YOU LOST! TRASH, GET OUT AND NEVER GUESS HOW MANY DICKS I HAVE EVER AGAIN";
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = '#ff4d4d';
+      backgroundColor('#ff4d4d');
     }
   }
 })
 
+//reset button
 document.querySelector('.again').addEventListener('click', function () {
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  secretNumber = randomGenerator;
   score = 4;
-  document.querySelector('.message').textContent = 'Start guessing Cock Count...';
+  displayMessage('Start guessing Cock Count...');
   document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
+  displayNumber('?');
   document.querySelector('.guess').value = '';
-
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  backgroundColor('#222');
+  widthNumber('15rem');
 })
+
+//patch notes
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const patchNotes = document.querySelector('.patch')
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+patchNotes.addEventListener('click', openModal);
+
+overlay.addEventListener('click', closeModal)
